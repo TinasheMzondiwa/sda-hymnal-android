ca4844b88ac6b33da93fadd5a56b29b37796c6dd
Tinashe Mzondiwa <tmzon08@gmail.com>
2025-08-20 23:13:41 -0400

    Add plus ext

--- libraries/core-ui/src/main/kotlin/hymnal/ui/extensions/PaddingValuesExt.kt
+++ libraries/core-ui/src/main/kotlin/hymnal/ui/extensions/PaddingValuesExt.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 
 fun PaddingValues.copy(
     layoutDirection: LayoutDirection,
@@ -15,6 +16,19 @@ fun PaddingValues.copy(
 ) = PaddingValues(
     start = start ?: calculateStartPadding(layoutDirection),
     top = top ?: calculateTopPadding(),
-    end = end ?: calculateEndPadding(LayoutDirection.Ltr),
+    end = end ?: calculateEndPadding(layoutDirection),
     bottom = bottom ?: calculateBottomPadding(),
+)
+
+fun PaddingValues.plus(
+    layoutDirection: LayoutDirection,
+    start: Dp? = null,
+    top: Dp? = null,
+    end: Dp? = null,
+    bottom: Dp? = null,
+) = PaddingValues(
+    start = calculateStartPadding(layoutDirection) + (start ?: 0.dp),
+    top = calculateTopPadding() + (top ?: 0.dp),
+    end = calculateEndPadding(layoutDirection) + (end ?: 0.dp),
+    bottom = calculateBottomPadding() + (bottom ?: 0.dp),
 )
