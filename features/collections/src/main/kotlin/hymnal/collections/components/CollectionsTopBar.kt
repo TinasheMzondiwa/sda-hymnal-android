403a7848264da7427077c7b78c1e90e536d17650
Tinashe Mzondiwa <tmzon08@gmail.com>
2025-09-08 00:20:06 -0400

    Replace LargeTopAppBar with CenterAlignedTopAppBar

--- features/collections/src/main/kotlin/hymnal/collections/components/CollectionsTopBar.kt
+++ features/collections/src/main/kotlin/hymnal/collections/components/CollectionsTopBar.kt
@@ -3,17 +3,8 @@
 
 package hymnal.collections.components
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -21,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import hymnal.libraries.l10n.R as L10nR
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,24 +20,8 @@ fun CollectionsTopBar(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
 ) {
-    LargeTopAppBar(
-        title = {
-            Column(Modifier.fillMaxWidth()) {
-                Text(stringResource(L10nR.string.collections))
-
-                AnimatedVisibility(scrollBehavior?.state?.collapsedFraction == 0f) {
-                    Spacer(
-                        Modifier
-                            .padding(top = 16.dp)
-                            .size(48.dp, 4.dp)
-                            .background(
-                                MaterialTheme.colorScheme.primary,
-                                RoundedCornerShape(4.dp)
-                            )
-                    )
-                }
-            }
-        },
+    CenterAlignedTopAppBar(
+        title = { Text(stringResource(L10nR.string.collections)) },
         modifier = modifier,
         scrollBehavior = scrollBehavior,
         colors = TopAppBarDefaults.topAppBarColors(
