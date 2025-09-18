fb631079d04f2f94fcd6afccd25da22aa0fbf19a
Tinashe Mzondiwa <tmzon08@gmail.com>
2025-09-17 22:29:44 -0400

    Switch to HorizontalPager and add vertical scrolling

--- features/sing/src/main/kotlin/hymnal/sing/immersive/ImmersiveContent.kt
+++ features/sing/src/main/kotlin/hymnal/sing/immersive/ImmersiveContent.kt
@@ -4,24 +4,23 @@
 package hymnal.sing.immersive
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.TextAutoSize
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -32,7 +31,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.slack.circuit.codegen.annotations.CircuitInject
 import dev.zacsweers.metro.AppScope
-import hymnal.ui.extensions.plus
 import hymnal.ui.theme.HymnalTheme
 import kotlinx.collections.immutable.persistentListOf
 import hymnal.sing.immersive.ImmersiveContentScreen.State as UiState
@@ -40,49 +38,46 @@ import hymnal.sing.immersive.ImmersiveContentScreen.State as UiState
 @CircuitInject(ImmersiveContentScreen::class, AppScope::class)
 @Composable
 fun ImmersiveContent(state: UiState, modifier: Modifier = Modifier) {
-    val layoutDirection = LocalLayoutDirection.current
     Scaffold (
         containerColor = Color.Black,
         contentColor = Color.White,
         contentWindowInsets = WindowInsets.safeDrawing,
     ) { contentPadding ->
-        VerticalPager(
+        HorizontalPager(
             state = rememberPagerState { state.pages.size },
             modifier = modifier.fillMaxSize(),
-            contentPadding = contentPadding.plus(
-                layoutDirection = layoutDirection,
-                start = 16.dp,
-                top = 32.dp,
-                end = 16.dp,
-                bottom = 32.dp
-            ),
+            contentPadding = contentPadding,
             pageSpacing = 32.dp,
         ) {
             val page = state.pages[it]
 
-            val text = buildAnnotatedString {
-                page.lines.forEachIndexed { index, line ->
-                    if (index > 0) {
-                        append("\n")
-                    }
+            val text = remember(page) {
+                buildAnnotatedString {
+                    page.lines.forEachIndexed { index, line ->
+                        if (index > 0) {
+                            append("\n")
+                        }
 
-                    if (index == 0) {
-                        withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                        if (index == 0) {
+                            withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                                append(line)
+                            }
+                        } else {
                             append(line)
                         }
-                    } else {
-                        append(line)
                     }
                 }
             }
 
             Box(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
                     text = text,
-                    modifier = Modifier,
+                    modifier = Modifier.padding(16.dp),
                     style = MaterialTheme.typography.bodyLarge,
                     textAlign = TextAlign.Center,
                     lineHeight = 48.sp,
