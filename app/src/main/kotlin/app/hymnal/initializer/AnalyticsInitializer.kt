e8262e5367ef5002365c74aa4e88fdc6333a6be2
Tinashe Mzondiwa <tmzon08@gmail.com>
2025-11-24 17:28:13 +0200

    Add custom Crashlytics keys

--- app/src/main/kotlin/app/hymnal/initializer/AnalyticsInitializer.kt
+++ app/src/main/kotlin/app/hymnal/initializer/AnalyticsInitializer.kt
@@ -6,14 +6,28 @@ package app.hymnal.initializer
 import android.content.Context
 import androidx.startup.Initializer
 import app.hymnal.BuildConfig
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.firebase.Firebase
 import com.google.firebase.analytics.analytics
+import com.google.firebase.crashlytics.crashlytics
+import com.google.firebase.crashlytics.setCustomKeys
 
 class AnalyticsInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
         with(Firebase.analytics) {
             setUserProperty("version_code", BuildConfig.VERSION_CODE.toString())
+        }
+
+        Firebase.crashlytics.setCustomKeys {
+            key("version_code", BuildConfig.VERSION_CODE.toString())
+            key(
+                "play_services_enabled", "${
+                    GoogleApiAvailability.getInstance()
+                        .isGooglePlayServicesAvailable(context) == ConnectionResult.SUCCESS
+                }"
+            )
         }
     }
 
